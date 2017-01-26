506315963c6bbdf753f6b4bd87c55a5f9bd7142e
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2017-01-26 16:53:28 +0000

    Inclusão do módulo para o gerenciamento do firewall

--- environments/development/manifests/site.pp
+++ environments/development/manifests/site.pp
@@ -1,3 +1,10 @@
 node default {
-		include zabbix
+		contain zabbix
+		contain firewall
+
+		#Provisionamento do Zabbix Server
+		Class['zabbix'] ->
+
+		#Provisionamento do Firewall
+		Class['firewall']
 }
